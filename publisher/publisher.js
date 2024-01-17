a3985324be5c4ddbe13d562305a24e0e520ca175
Abdel Majid Maalej <103139965+AbdelMajidMaalej@users.noreply.github.com>
2024-01-17 13:26:02 +0100

    IOT

--- publisher/publisher.js
+++ publisher/publisher.js
@@ -1,15 +1,26 @@
+// publisher.js
+
+// Importe la bibliothèque MQTT
 const mqtt = require("mqtt");
 
+// URL du courtier MQTT (broker)
 const brokerUrl = "mqtt://test.mosquitto.org";
 
+// Sujet (topic) sur lequel les données seront publiées
 const topic = "Temp";
 
+// Crée un client MQTT en se connectant au courtier
 const client = mqtt.connect(brokerUrl);
 
+// Lorsque la connexion avec le courtier est établie
 client.on("connect", () => {
+    // Met en place une fonction qui s'exécute toutes les 5000 millisecondes (5 secondes)
   setInterval(() => {
+        // Génère une température aléatoire entre 0 et 150 degrés Celsius
     const temperature = Math.floor(Math.random() * 150);
+        // Publie la température sur le sujet (topic) spécifié
     client.publish(topic, temperature.toString());
+        // Affiche un message dans la console indiquant la température publiée
     console.log(`Température publiée: ${temperature} °C`);
   }, 5000);
 });
--- subscriber/subscriber.js
+++ subscriber/subscriber.js
@@ -1,19 +1,28 @@
 // subscriber.js
 
+// Importation des modules requis
 const express = require("express");
 const mqtt = require("mqtt");
 const http = require('http');
 const socketIO = require("socket.io");
+
+// URL du courtier MQTT et le sujet (topic) à écouter
 const brokerUrl = "mqtt://test.mosquitto.org";
 const topic = "Temp";
 
+// Initialisation d'une application Express
 const app = express();
-const port = 3000;
-const webPort = 5000;
+const port = 3000;// Port pour l'application Express
+const webPort = 5000;// Port pour le serveur WebSocket
 
+// Création du serveur HTTP
 const server = http.createServer(app);
 server.listen(webPort)
+
+// Connexion au courtier MQTT
 const client = mqtt.connect(brokerUrl);
+
+// Initialisation du serveur WebSocket avec CORS activé
 const io = socketIO(server, {
   cors: {
     origin: "*",
@@ -21,19 +30,24 @@ const io = socketIO(server, {
   }
 });
 
+// Gestionnaire d'événement lorsque le client se connecte au serveur WebSocket
 io.on('connection', (socket) => {
   console.log('Client connected');
 
+  // Gestionnaire d'événement lorsque le client se déconnecte du serveur WebSocket
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
 
+// Route pour la page d'accueil de l'application
 app.get("/", (req, res) => {
   res.send("Application Subscriber");
 });
 
+// Gestionnaire d'événement lors de la connexion au courtier MQTT
 client.on("connect", () => {
+    // Abonnement au sujet (topic) spécifié
   client.subscribe(topic, (err) => {
     if (!err) {
       console.log(`Connecté au broker MQTT. Écoute du topic "${topic}"...`);
@@ -41,16 +55,18 @@ client.on("connect", () => {
   });
 });
 
+// Gestionnaire d'événement lors de la réception d'un message depuis le courtier MQTT
 client.on("message", (topic, message) => {
   if (topic === "Temp") {
     const temperature = message.toString();
     console.log(`Température reçue: ${temperature} °C`);
-    // Ici, vous pourriez mettre en œuvre la logique d'affichage sur une interface web.
+    // Émission du message aux clients connectés via le serveur WebSocket
     io.emit('serverMessage', temperature);
 
   }
 });
 
+// Démarrage du serveur Express
 app.listen(port, () => {
   console.log(`Serveur Express en cours d'exécution sur le port ${port}`);
 });
